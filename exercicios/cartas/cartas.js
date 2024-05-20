2186e953450ea5fb0c1606716ee5c4afcdce7730
LUIS ANTONIO DE PAULA LIMA <luis.aplima@senacsp.edu.br>
2024-05-20 17:18:52 -0300

    cartas

---
+++ exercicios/cartas/cartas.js
@@ -0,0 +1,46 @@
+/**
+ * Sorteio de uma carta
+ * Exemplo de uso do array para simplificar o código
+ * @author Luís Antônio de Paula Lima
+ */
+
+function sortearCarta() {
+
+    //           [0]   [1]   [2]   [3] 
+    let nipes = ['♦' , '♥' , '♠' , '♣']
+    //           [0]   [1]   [2]   [3]   [4]   [5]   [6]   [7]   [8]    [9]   [10]  [11]  [12]
+    let faces = ['A' , '2' , '3' , '4' , '5' , '6' , '7' , '8' , '9' , '10' , 'J' , 'Q' , 'K']
+
+    //sorteio do indice dos vetores
+    let nipe = nipes[Math.floor(Math.random() * 4)] //(0 - 3)
+    let face = faces[Math.floor(Math.random() * 13)] //(0 - 12)
+
+    //console.log(`${face}${nipe}`)
+
+    // Determinar a cor com base no nipe sorteado antes de exibir a carta
+    let cor
+    if (nipe === '♥' || nipe === '♦') {
+        cor = '#ff0000' //vermelho
+    } else {
+        cor = '#000000' //preto
+    }
+
+    // Renderizar (desenhar, exibir, "dar vida") a carta
+
+    document.getElementById('supEsq').innerHTML = `<div>${face}</div><div>${nipe}</div>`
+    document.getElementById('centro').innerHTML = `<div>${nipe}</div>`
+    document.getElementById('infDir').innerHTML = `<div>${face}</div><div>${nipe}</div>`
+
+    if (face === 'K') {
+        
+    } else if (face === 'Q') {
+
+    } else if (face === 'J') {
+
+    }
+
+    // Aplicar a cor
+    document.getElementById('supEsq').style.color = cor /* Adicionar CSS */
+    document.getElementById('centro').style.color = cor /* Adicionar CSS */
+    document.getElementById('infDir').style.color = cor /* Adicionar CSS */
+}
