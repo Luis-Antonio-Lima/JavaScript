0a8cc4b4ac2f503745f82d08eaad679fb6baddc4
LUIS ANTONIO DE PAULA LIMA <luis.aplima@senacsp.edu.br>
2024-05-20 14:36:23 -0300

    pdv

--- pdv/pdv.js
+++ pdv/pdv.js
@@ -3,31 +3,32 @@
  * @author Luís Antônio de Paula Lima
  */
 
-let valor, desconto, troco, valorDesconto, valorPago, rs, rs2
+//variáveis
+let valor, desconto, troco, valorPago, rs, rs2
 
 function calcular() {
-
+    //entrada
     valor = Number(frmPdv.txtTotal.value)
     desconto = Number(frmPdv.txtPorcentagem.value)
 
+    //processamento
     if (desconto >= 1) {
         rs = (valor * desconto) / 100
-        frmPdv.txtDesconto.value = rs.toFixed(0)
+        frmPdv.txtDesconto.value = `R$ ${rs.toFixed(0)}` //saida
         rs2 = valor - rs
-        frmPdv.txtTotalDesconto.value = rs2.toFixed(2)
+        frmPdv.txtTotalDesconto.value = `R$ ${rs2.toFixed(2)}` //saida
     } else {
-        frmPdv.txtTotalDesconto.value = valor.toFixed(2)
-        frmPdv.txtDesconto.value = desconto.toFixed(0)
+        frmPdv.txtTotalDesconto.value = `R$ ${valor.toFixed(2)}` //saida
+        frmPdv.txtDesconto.value = `R$ ${desconto.toFixed(0)}` //saida
         rs2 = valor
     }
 
 }
 
 function Calctroco() {
+    //processamento
     valorPago = Number(frmPdv.txtValorPago.value)
 
     troco = valorPago - rs2
-    frmPdv.txtTroco.value = troco.toFixed(2)
+    frmPdv.txtTroco.value = `R$ ${troco.toFixed(2)}` //saida
 }
-
-function limpar() {}
