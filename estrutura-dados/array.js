2580defea40e499fa42e44896c748fbece8c119d
LUIS ANTONIO DE PAULA LIMA <luis.aplima@senacsp.edu.br>
2024-05-22 15:30:33 -0300

    array

---
+++ estrutura-dados/array.js
@@ -0,0 +1,136 @@
+/**
+ * Estrutura de dados
+ * Array (vetor e matriz)
+ * @author Luís Antônio de Paula Lima
+ */
+
+let alunosEM1 = ['Vitor','Tania','Pedro','Maria','Viviane','Sirlene']
+console.log(typeof alunosEM1)
+
+//determinando o tamanho de uma array
+console.log(alunosEM1.length)
+
+// exibindo os dados de uma array (CRUD Create)
+console.log(alunosEM1[1])
+console.log(alunosEM1)
+console.table(alunosEM1)
+
+//Adicionando dados ao array (CRUD Read)
+alunosEM1.push('Jorge')
+console.table(alunosEM1)
+
+// Alterando um dado do array (CRUD update)
+alunosEM1[0] = 'Victor'
+console.table(alunosEM1)
+
+//Excluindo um dado do array
+alunosEM1.pop() //exclui o ultimo registro
+console.table(alunosEM1)
+
+delete alunosEM1[3] //exclui um dado especificado pelo indice
+console.table(alunosEM1)
+
+// Percorrendo um array
+let notas =[3, 8, 5, 9, 2]
+console.log(notas)
+console.table(notas)
+
+//laço for
+for (let i=0; i < notas.length; i++) {
+    console.log(notas[i])
+}
+
+//forEach
+notas.forEach((n) => {
+    console.log(n)
+})
+
+//map()
+//Exemplo 1: Adicionar 1 ponto as notas
+let notasAtualizadas = notas.map((na) => {
+    return na + 1
+})
+console.log(notas)
+console.log(notasAtualizadas)
+
+//Exemplo 2: Conversão do sistema de pontos para letras
+//NA (não atendeu (nota < 5))
+//PA (parcialmente atendido (nota entre 5 e 7))
+//A (atendeu (nota > 7))
+let notasConvertidas = notas.map((nc) => {
+    if (nc < 5) {
+        return ('NA')
+    } else if (nc > 7) {
+        return ('A')
+    } else {
+        return ('PA')
+    }
+})
+console.log(notas)
+console.log(notasConvertidas)
+
+// Iniciando um array como um objeto
+console.clear()
+let alunosEM2 = new Array('Tony','Peter','Thor','Natasha')
+console.log(alunosEM2)
+console.table(alunosEM2)
+
+//estrutura de dados
+let alunosEM3 = [
+    {
+        nome: 'Bruce',
+        idade: 43,
+        bolsista: false
+    },
+    {
+        nome: 'Clark',
+        idade: 45,
+        bolsista: false
+    },
+    {
+        nome: 'Diana',
+        idade: 36,
+        bolsista: false
+    },
+    {
+        nome: 'Barry',
+        idade: 23,
+        bolsista: true
+    }
+]
+
+console.log(alunosEM3)
+console.table(alunosEM3)
+
+//filtros
+//Ordenar de A - Z
+alunosEM3.sort((a, z) => {
+    return (a.nome.localeCompare(z.nome))
+})
+console.table(alunosEM3)
+
+//Ordenar por idade
+alunosEM3.sort((x, y) => {
+    return (x.idade - y.idade)
+})
+console.table(alunosEM3)
+
+//ATENÇÃO (criando uma cópia para exibição)
+let alunosOrdenados = [...alunosEM3]
+
+//Ordenar de A - Z
+alunosOrdenados.sort((a, z) => {
+    return (a.nome.localeCompare(z.nome))
+})
+console.table(alunosOrdenados)
+
+//Buscas personalizadas em um array
+//Ex 1: Alunos bolsistas
+console.table(alunosEM3.filter((b) => {
+    return b.bolsista === true
+}))
+
+//Ex 2: Alunos com idade superior a 40 anos
+console.table(alunosEM3.filter((i) => {
+    return i.idade > 40
+}))
